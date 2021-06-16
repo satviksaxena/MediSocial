17e2fe41ee148227a782fa53caea6bf74d7d70eb
satviksaxena <satviksaxena1@gmail.com>
2021-06-17 00:39:54 +0530

    index changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-hi
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+app.get('/home', function(req, res) {
+    res.send('<h1>YO supp</h1>');
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log('Error in running the server', err);
+    }
+    console.log("Yay! its Alive!!", port);
+});
