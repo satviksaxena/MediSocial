fcea7bc2ffca6b9c7424c1d81038bab5460bfcd6
satviksaxena <satviksaxena1@gmail.com>
2021-06-28 23:33:43 +0530

    till setup db

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const app = express();
 const port = 8000;
 
-const app = express();
+const path = require('path');
 
-app.get('/home', function(req, res) {
-    res.send('<h1>YO supp!!</h1>');
-    // console.log(req);
-    //cheking the push 
-
-});
-
+//setup ejs views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//setup layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+//setup assets
+app.use(express.static('./assets'));
+//route
+app.use('/', require('./routes/index'));
+//db
+const db = require('./config/mongoose');
 app.listen(port, function(err) {
     if (err) {
         console.log('Error in running the server', err);
